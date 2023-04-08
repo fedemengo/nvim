653a8c85af61f6b679849a75495998b5884858dc
Federico Mengozzi <19249682+fedemengo@users.noreply.github.com>
2023-04-08 13:36:34 +0200

    clean up

--- init.lua
+++ init.lua
@@ -5,26 +5,15 @@ local function ensure(user, repo)
 		print("git cloning " .. string.format("github.com/%s/%s", user, repo))
 		vim.fn.execute(string.format("!git clone https://github.com/%s/%s %s", user, repo, install_path))
 		vim.fn.execute(string.format("packadd %s", repo))
-		return true
 	end
-	return false
 end
 
 ensure("wbthomason", "packer.nvim")
-ensure("lewis6991", "impatient.nvim")
 ensure("Olical", "aniseed")
-
-ensure("NLKNguyen", "papercolor-theme")
-ensure("fedemengo", "github-nvim-theme")
-
+ensure("lewis6991", "impatient.nvim")
 require("impatient").enable_profile()
 
 -- require("mods/dev/profile")
---
--- local log = require("mods/dev/log").setup({outfile = "/tmp/nvim.log", color = true})
--- log.trace("Hello world", { "foo", "bar", key ="value" }, 1, 2, 3)
 
-vim.g["aniseed#env"] = {
-	module = "init",
-	compile = true,
-}
+vim.g["aniseed#env"] = { module = "init", compile = true }
+
